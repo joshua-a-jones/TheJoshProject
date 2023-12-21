8f3e09e0ee35e12bb611d2476b4ef3e57d2b50fb
Joshua Jones <joshua.desjones@gmail.com>
2023-12-20 20:07:41 -0600

    add education controller

---
+++ TheJoshProject.Api/Controllers/EducationController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using TheJoshProject.Api.Models;
+using TheJoshProject.Api.Services;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class EducationController : ControllerBase
+{
+    private readonly IEducationService _educationService;
+
+    public EducationController(IEducationService educationService)
+    {
+        _educationService = educationService;
+    }
+
+    /// <summary>
+    /// Get all education
+    /// </summary>
+    /// <returns></returns>
+    /// <response code="200">Returns skills for education id</response>
+    /// <response code="404">If education id is not found</response>
+    /// <response code="500">If there is an error</response>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/education/all
+    ///     
+    /// </remarks>
+    [HttpGet("all")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult>GetAllEducationAsync()
+    {
+        var result = await _educationService.GetAllEducationAsync();
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Get education by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <response code="200">Returns skills for education id</response>
+    /// <response code="404">If education id is not found</response>
+    /// <response code="500">If there is an error</response>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/education?id=1
+    ///     
+    /// </remarks>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetEducation([FromQuery] int id)
+    {
+        var education =  await _educationService.GetEducationByIdAsync(id);
+
+        if (education == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(education);
+    }
+}
