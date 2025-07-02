aea688b5e7f07f58a94020f3aca1b7a156d363f9
Joshua Jones <joshua.desjones@gmail.com>
2025-07-02 16:02:17 -0400

    Add structured logging

--- TheJoshProject.Api/Services/EmployerService.cs
+++ TheJoshProject.Api/Services/EmployerService.cs
@@ -1,28 +1,34 @@
 using TheJoshProject.Api.Models;
 namespace TheJoshProject.Api.Services;
 using TheJoshProject.Api.DataAccess.Repositories;
+using Microsoft.Extensions.Logging;
 
 public class EmployerService : IEmployerService
 {
     private readonly IEmployerRepository _employerRepository;
+    private readonly ILogger<EmployerService> _logger;
 
-    public EmployerService(IEmployerRepository employerRepository)
+    public EmployerService(IEmployerRepository employerRepository, ILogger<EmployerService> logger)
     {
         _employerRepository = employerRepository;
+        _logger = logger;
     }
 
     public async Task<List<Employer>> GetAllEmployers()
     {
+        _logger.LogInformation("Retrieving all employers");
         return await _employerRepository.GetAllAsync();
     }
 
     public async Task<Employer?> GetEmployer(int id)
     {
+        _logger.LogInformation("Retrieving employer with id {Id}", id);
         return await _employerRepository.GetByIdAsync(id);
     }
 
     public async Task<int> GetEmployerIdByName(string employerName)
     {
+        _logger.LogInformation("Retrieving employer id for {Name}", employerName);
         return await _employerRepository.GetIdByNameAsync(employerName);
     }
 }
